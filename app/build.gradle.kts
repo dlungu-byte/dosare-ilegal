f15fb49d533bf158ef88f005b744efb1f83a8f51
dlungu-byte <lgulearn@gmail.com>
2026-09-10 22:52:21 +0300

    Compile against Android API 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.consultrio.quitplan"
-    compileSdk = 36
+    compileSdk = 37
     defaultConfig {
         applicationId = "com.consultrio.quitplan"
         minSdk = 26
